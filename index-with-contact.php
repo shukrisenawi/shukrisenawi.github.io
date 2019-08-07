bd04212780c895b1f0153f3df0c40048482db0ed
shukrisenawi <shukrisenawi@gmail.com>
2019-08-07 14:56:26 +0800

    baru pasang theme

--- index-with-contact.php
+++ index-with-contact.php
@@ -1,3 +1,43 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+$headers .= "From:".strtolower($_POST['email'])."\r\n";
+$headers .= "X-Sender: $_POST[email]\n";
+$headers .= "Return-Path: $_POST[email]\n";
+$headers .= "MIME-Version: 1.0\n";
+$headers .= "Content-type: text/html; charset=utf-8\r\n";
+
+$enviar = "<html>
+                <head>
+                <title>CONTATO</title>
+                </head>
+                <body>
+                <table width='100%' cellpadding='0' cellspacing='0' border='0' style='background-color:#677DA3;'>
+                  <tr>
+                    <td>
+                        <table width='100%' cellpadding='4' cellspacing='1' bgcolor='#CCCCCC' style='font-family:tahoma, arial; font-size:11px; color:#666666;'>
+                            <tr>  
+                                <td width='15%' height='25' bgcolor='#F3F3F3'><strong>Name:</strong></td>
+                                <td bgcolor='#FFFFFF'>$_POST[name]</td>
+                            </tr>
+                            <tr>  
+                                <td width='15%' height='25' bgcolor='#F3F3F3'><strong>Email:</strong></td>
+                                <td bgcolor='#FFFFFF'>".strtolower($_POST['email'])."</td>
+                            </tr>
+                            <tr>  
+                                <td width='15%' height='25' bgcolor='#F3F3F3'><strong>Message:</strong></td>
+                                <td bgcolor='#FFFFFF'>".nl2br($_POST['message'])."</td>
+                            </tr>                           
+                        </table>
+                    </td>
+                  </tr>
+                </table>
+            </body>
+            </html>";
+    mail("thiago@moch.com.br","[Contact]", $enviar, $headers);          
+    echo "<script language='javascript'>alert('Mensagem enviada com sucesso! Aguarde o nosso contato');location.href='index.html'</script>";
+}
+?>
 <!doctype html>
 <!--[if lt IE 7 ]> <html lang="pt-BR" xmlns:fb="http://ogp.me/ns/fb#" class="no-js oldie ie6 lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7 ]>    <html lang="pt-BR" xmlns:fb="http://ogp.me/ns/fb#" class="no-js oldie ie7 lt-ie9 lt-ie8"> <![endif]-->
@@ -317,18 +357,18 @@
                 <div class="fr">
                     <section id="contact-me">
                         <h2>Contact Me</h2>
-                            <form id="form-contact-me" name="form-contact-me" action="#">
-                              <dl>
-                                <dt>Your Name</dt>
-                                <dd><input type="text" name="name" id="name" class="field f1" value="" /></dd>
-                                <dt>Your Email</dt>
-                                <dd><input type="text" name="email" id="email" class="field f1" value="" /></dd>
-                                <dt>Your Message</dt>
-                                <dd><textarea name="message" id="message" class="field f2"></textarea></dd>
-                                <dt>&nbsp;</dt>
-                                <dd><input type="submit" value="Send Message" /></dd>
-                              </dl>
-                            </form>
+                            <form id="form-contact-me" method="post" name="form-contact-me" action="index-with-contact.php">
+                          <dl>
+                            <dt>Your Name</dt>
+                            <dd><input type="text" name="name" id="name" class="field f1" value="" /></dd>
+                            <dt>Your Email</dt>
+                            <dd><input type="text" name="email" id="email" class="field f1" value="" /></dd>
+                            <dt>Your Message</dt>
+                            <dd><textarea name="message" id="message" class="field f2"></textarea></dd>
+                            <dt>&nbsp;</dt>
+                            <dd><input type="submit" value="Send Message" /></dd>
+                          </dl>
+                        </form>
                     </section>
                 </div>
             </section>
